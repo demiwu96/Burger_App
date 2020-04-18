a2c98cf088552499748060d4b64f497410cc31f5
Demi Wu <demiwu96@gmail.com>
2020-04-18 16:16:13 -0400

    add config files

---
+++ config/orm.js
@@ -0,0 +1,19 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+    all: function () {
+
+    },
+    insert: function () {
+
+    },
+    update: function () {
+
+    },
+    name: function () {
+
+    },
+}
+
+// Export the orm object
+module.exports = orm;
