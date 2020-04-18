2d163c67ef18f988ff3ba67612ffa5017b363812
Demi Wu <demiwu96@gmail.com>
2020-04-18 16:15:27 -0400

    add sql files

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS burgerorders_db;
+CREATE DATABASE burgerorders_db;
+
+USE burgerorders_db;
+
+CREATE TABLE burgers(
+id INT AUTO_INCREMENT NOT NULL,
+burger_name VARCHAR(200),
+devoured BOOLEAN DEFAULT FALSE,
+PRIMARY KEY (id)
+);
