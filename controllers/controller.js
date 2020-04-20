56e70335c9b02f463ffe5e6270e125ffbbe9c87e
Demi Wu <demiwu96@gmail.com>
2020-04-20 10:34:24 -0400

    devour function

--- controllers/controller.js
+++ controllers/controller.js
@@ -11,13 +11,27 @@ router.get("/", function (req, res) {
     });
 });
 
-router.post("/api/burgers", function(req, res) {
+router.post("/api/burgers", function (req, res) {
     let name = req.body.name;
 
-    burger.insert(name, function(result) {
-      // Send back the ID
-      res.json({ id: result.insertId });
+    burger.insert(name, function (result) {
+        // Send back the ID
+        res.json({ id: result.insertId });
     });
-  });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    let value = req.body.devoured;
+    console.log("condition", condition);
+
+    burger.update(value, condition, function (result) {
+        if (result.changedRows === 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        }
+        res.status(200).end();
+    });
+});
 
 module.exports = router;
--- public/assets/burger-logic.js
+++ public/assets/burger-logic.js
@@ -1,20 +1,43 @@
-$(".orderBurger-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
+$(function () {
+    $(".orderBurger-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
 
-    let newBurger = {
-      name: $("#burger").val().trim()
-    };
+        let newBurger = {
+            name: $("#burger").val().trim()
+        };
 
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(
-      function() {
-        console.log("ordered a burger");
-        // Reload the page to get the updated list
-        location.reload();
-      }
-    );
-  });
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("ordered a burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".devourBtn").click(function (event) {
+        var id = $(this).data("burgerid");
+
+        var devouredStatus = {
+            devoured: true
+        };
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: devouredStatus
+        }).then(
+            function () {
+                console.log("the burger has been eaten");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+
+});
+
