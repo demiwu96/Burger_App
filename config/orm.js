01b92136f708fb80391f60541dcc6c6841fefdbe
Demi Wu <demiwu96@gmail.com>
2020-04-20 09:15:51 -0400

    insert and update functions

--- config/orm.js
+++ config/orm.js
@@ -1,24 +1,39 @@
 var connection = require("../config/connection.js");
 
 var orm = {
-    selectAll: function (tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function(err, result) {
-          if (err) {
-            throw err;
-          }
-          cb(result);
+    selectAll: function (table, cb) {
+        let queryString = "SELECT * FROM " + table + ";";
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
         });
-      },
-    insert: function () {
-
     },
-    update: function () {
 
-    },
-    name: function () {
+    insertInto: function (table, cols, vals, cb) {
+        let queryString = "INSERT INTO " + table + " SET " + cols + " = ?";
+        console.log(queryString);
 
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
     },
+
+    update: function (table, cols, vals, condition, cb) {
+        var queryString = "UPDATE " + table + " SET " + cols + " = " + vals + " WHERE " + condition;
+        console.log(queryString);
+
+        connection.query(queryString, function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
 }
 
 // Export the orm object
