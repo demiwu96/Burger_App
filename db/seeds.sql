a59c49971f178f621a824a73243457907b7abd46
Demi Wu <demiwu96@gmail.com>
2020-04-20 13:39:28 -0400

    updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,5 +3,7 @@ USE burgerorders_db;
 INSERT INTO burgers (burger_name) VALUE ('angus beef burger');
 INSERT INTO burgers (burger_name) VALUE ('cod burger');
 INSERT INTO burgers (burger_name) VALUE ('bacon and cheese burger');
+INSERT INTO burgers (burger_name) VALUE ('double cheese burger');
 INSERT INTO burgers (burger_name) VALUE ('crispy chicken burger');
 INSERT INTO burgers (burger_name) VALUE ('grilled chicken burger');
+INSERT INTO burgers (burger_name) VALUE ('veggie burger');
