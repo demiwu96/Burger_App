6b23df47a1eea718cc5c6ef9ccc8210513c99ea8
Demi Wu <demiwu96@gmail.com>
2020-04-20 20:58:25 -0400

    add JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Demiwu-0912",
   database: "burgerorders_db"
 });
+};
 
 // Make connection.
 connection.connect(function(err) {
